7103fc953a79f2b1a4f510160da4691c6d17288b
rodrigosa <rodrigo.gomes.sa@outlook.pt>
2026-04-18 02:12:04 +0100

    feat(auth): add shared module with CurrentUser decorator

---
+++ src/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { Global, Module } from "@nestjs/common";
+
+import { CurrentUserService } from "./current-user.service";
+
+@Global()
+@Module({
+  providers: [CurrentUserService],
+  exports: [CurrentUserService],
+})
+export class SharedModule {}
